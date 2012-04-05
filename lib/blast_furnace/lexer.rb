2063cb2d2dd793de8c9e6faff4663961002a93c7
Mark Ryall <mark@ryall.name>
2012-04-05 17:42:53 +1000

    working lexer

--- lib/blast_furnace/lexer.rb
+++ lib/blast_furnace/lexer.rb
@@ -2,4 +2,27 @@ module BlastFurnace
 end
 
 class BlastFurnace::Lexer
+  KEYWORDS = %w{class}
+  def initialize string
+    @fiber = Fiber.new do
+      index = 0
+      while index < string.size
+        case string[index..-1]
+        when /\A([A-Za-z]\w*)/
+          if KEYWORDS.include? $1
+            Fiber.yield [$1.to_sym, nil]
+          else
+            Fiber.yield [:identifier, $1]
+          end
+          index+=$1.size
+        else
+          index+=1
+        end
+      end
+    end
+  end
+
+  def next
+    @fiber.resume
+  end
 end
--- spec/blast_furnace/lexer_spec.rb
+++ spec/blast_furnace/lexer_spec.rb
@@ -3,5 +3,16 @@ require_relative '../spec_helper'
 require 'blast_furnace/lexer'
 
 describe BlastFurnace::Lexer do
+  def lexer code=nil
+    @lexer = BlastFurnace::Lexer.new code unless @lexer
+    @lexer
+  end
 
+  it "should tokenise class declaration" do
+    lexer <<-EOF
+    class AClass
+    EOF
+    lexer.next.should == [:class, nil]
+    lexer.next.should == [:identifier, "AClass"]
+  end
 end
