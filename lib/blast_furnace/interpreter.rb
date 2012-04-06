4a110a3bfba8c61d92202ece5cd94ad26209fe24
Mark Ryall <mark@ryall.name>
2012-04-06 11:50:00 +1000

    added (hard coded) class generation to (not at all useful)  interpreter

--- lib/blast_furnace/interpreter.rb
+++ lib/blast_furnace/interpreter.rb
@@ -2,4 +2,12 @@ module BlastFurnace
 end
 
 class BlastFurnace::Interpreter
+  def interpret *expressions
+    expressions.each do |expression|
+      puts <<EOF
+public class #{expression.last} {
+}
+EOF
+    end
+  end
 end
--- spec/blast_furnace/interpreter_spec.rb
+++ spec/blast_furnace/interpreter_spec.rb
@@ -1,4 +1,22 @@
 require 'blast_furnace/interpreter'
 
 describe BlastFurnace::Interpreter do
+  let(:interpreter) { BlastFurnace::Interpreter.new }
+
+  def interpret *tokens
+    interpreter.interpret *tokens
+  end
+
+  it 'should generate nothing from empty token list' do
+    interpreter.should_not_receive :puts
+    interpret
+  end
+
+  it 'should generate simple class' do
+    interpreter.should_receive(:puts).with <<EOF
+public class AClass {
+}
+EOF
+    interpret [:class, 'AClass']
+  end
 end
