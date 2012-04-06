c32fc0451cbd8ef6cc3eb134091bae5af4553e2c
Mark Ryall <mark@ryall.name>
2012-04-06 12:47:36 +1000

    got basic end to end functioanality working

--- lib/blast_furnace/blaster.rb
+++ lib/blast_furnace/blaster.rb
@@ -1,7 +1,16 @@
+require 'blast_furnace/interpreter'
+require 'blast_furnace/parser'
+require 'blast_furnace/lexer'
+
 module BlastFurnace
   class Blaster
-    def blast
-      puts 'KAPOW!'
+    def blast *args
+      destination = args.shift
+      parser = BlastFurnace::Parser.new
+      args.each do |path|
+        lexer = BlastFurnace::Lexer.new File.read path
+        BlastFurnace::Interpreter.new.interpret destination, *parser.parse(lexer)
+      end
     end
   end
 end
